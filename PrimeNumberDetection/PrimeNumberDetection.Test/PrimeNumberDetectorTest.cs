0bbd8975acf57384473bee60d118c07f6ce8d7d9
Mate Bicserdi <mate.bicserdi@harman.com>
2024-05-16 14:40:35 +0200

    A possible solution with tests.

--- PrimeNumberDetection/PrimeNumberDetection.Test/PrimeNumberDetectorTest.cs
+++ PrimeNumberDetection/PrimeNumberDetection.Test/PrimeNumberDetectorTest.cs
@@ -3,18 +3,78 @@ namespace PrimeNumberDetection.Test
     [TestClass]
     public class PrimeNumberDetectorTest
     {
-        [TestMethod]
-        public void IsDivisibleByTwo()
+        PrimeNumberDetector primeNumberDetector;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
-            PrimeNumberDetector primeNumberDetector = new PrimeNumberDetector();
-            Assert.IsFalse(primeNumberDetector.IsItPrimeNumber(2));
+            primeNumberDetector = new PrimeNumberDetector();
         }
 
         [TestMethod]
-        public void IsDivisibleByThree()
+        public void IsItPrime_1_RetrunsFalse()
         {
-            PrimeNumberDetector primeNumberDetector = new PrimeNumberDetector();
-            Assert.IsTrue(primeNumberDetector.IsItPrimeNumber(3));
+            Assert.IsFalse(primeNumberDetector.IsItPrime(1));
+        }
+
+        [TestMethod]
+        public void IsItPrime_2_RetrunsTrue()
+        {
+            Assert.IsTrue(primeNumberDetector.IsItPrime(2));
+        }
+
+        [TestMethod]
+        public void IsItPrime_3_RetrunsTrue()
+        {
+            Assert.IsTrue(primeNumberDetector.IsItPrime(3));
+        }
+
+        [TestMethod]
+        public void IsItPrime_4_RetrunsFalse()
+        {
+            Assert.IsFalse(primeNumberDetector.IsItPrime(4));
+        }
+
+        [TestMethod]
+        public void IsItPrime_5_RetrunsTrue()
+        {
+            Assert.IsTrue(primeNumberDetector.IsItPrime(5));
+        }
+
+        [TestMethod]
+        public void IsItPrime_7_RetrunsTrue()
+        {
+            Assert.IsTrue(primeNumberDetector.IsItPrime(7));
+        }
+
+        [TestMethod]
+        public void IsItPrime_10_RetrunsFalse()
+        {
+            Assert.IsFalse(primeNumberDetector.IsItPrime(10));
+        }
+
+        [TestMethod]
+        public void IsItPrime_89_RetrunsFalse()
+        {
+            Assert.IsTrue(primeNumberDetector.IsItPrime(89));
+        }
+ 
+
+        [TestMethod]
+        public void IsItPrime_From_1_To_100()
+        {
+            int[] primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+            bool expected = false;
+
+
+            for (int i = 1; i <= 100; i++) 
+            {
+                if (primes.Contains(i)) { expected = true; }
+                else { expected = false; }
+
+                Assert.AreEqual(expected, primeNumberDetector.IsItPrime(i));                
+            }
         }
 
     }
--- PrimeNumberDetection/PrimeNumberDetection/PrimeNumberDetector.cs
+++ PrimeNumberDetection/PrimeNumberDetection/PrimeNumberDetector.cs
@@ -2,10 +2,19 @@
 {
     public class PrimeNumberDetector
     {
-        public bool IsItPrimeNumber(int number)
+        public bool IsItPrime(int number)
         {
+            if (number == 1 || number == 0) 
+            {
+                return false;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
             return true;
         }
-
     }
 }
